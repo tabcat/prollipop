917a501bfc7472975fdf7adc6e3c5643a5520658
tabcat <tabcat00@proton.me>
2024-06-08 13:47:34 -0500

    refactor: DefaultNode class, codec param
    Replaces Node class with DefaultNode class. Also adds Node interface back.
    Encode/decode functions take a codec parameter.

--- src/node.ts
+++ src/node.ts
@@ -1,26 +1,33 @@
-import * as cbor from "cborg";
 import { compare as compareHash } from "uint8arrays";
 import type { ByteView } from "multiformats/interface";
-import { decodeOptions, encodeOptions } from "@ipld/dag-cbor";
+import { BlockCodecPlus } from "./codec";
 
 export interface Tuple {
   timestamp: number;
   hash: Uint8Array;
 }
 
-export class Node implements Tuple {
+export interface Node extends Tuple {
+  message: Uint8Array;
+  asBytes<Code extends number>(
+    codec: BlockCodecPlus<Code, EncodedNode>
+  ): Uint8Array;
+}
+
+export class DefaultNode implements Node {
   #bytes: Uint8Array;
 
   constructor(
     readonly timestamp: number,
     readonly hash: Uint8Array,
-    // Branch node messages will always be SHA256 digest of the child bucket.
     readonly message: Uint8Array
   ) {}
 
-  asBytes (): Uint8Array {
+  asBytes<Code extends number>(
+    codec: BlockCodecPlus<Code, EncodedNode>
+  ): Uint8Array {
     if (this.#bytes == null) {
-      encode(this.timestamp, this.hash, this.message);
+      this.#bytes = encode(this.timestamp, this.hash, this.message, codec);
     }
 
     return this.#bytes;
@@ -30,24 +37,24 @@ export class Node implements Tuple {
 type EncodedTuple = [Tuple["timestamp"], Tuple["hash"]];
 export type EncodedNode = [...EncodedTuple, Node["message"]];
 
-export function encode(
+export function encode<Code extends number>(
   timestamp: number,
   hash: Uint8Array,
-  message: Uint8Array
+  message: Uint8Array,
+  codec: BlockCodecPlus<Code, EncodedNode>
 ): ByteView<EncodedNode> {
-  return cbor.encode([timestamp, hash, message], encodeOptions);
+  return codec.encode([timestamp, hash, message]);
 }
 
-export function decodeFirst(
-  bytes: ByteView<unknown>
-): [Node, Uint8Array] {
-  const [decoded, remainder]: [unknown, Uint8Array] =
-    cbor.decodeFirst(bytes, decodeOptions);
+export function decodeFirst<Code extends number>(
+  bytes: ByteView<EncodedNode[]>,
+  codec: BlockCodecPlus<Code, EncodedNode>
+): [DefaultNode, Uint8Array] {
+  const [decoded, remainder] = codec.decodeFirst(bytes);
 
-  // do verification here
-  const [timestamp, hash, message]: EncodedNode = decoded as any
+  // do verification on decoded here
 
-  return [new Node(timestamp, hash, message), remainder];
+  return [new DefaultNode(...decoded), remainder];
 }
 
 export const extractTuple = ({ timestamp, hash }: Tuple): Tuple => ({
@@ -55,7 +62,7 @@ export const extractTuple = ({ timestamp, hash }: Tuple): Tuple => ({
   hash,
 });
 
-export const compare = (a: Node, b: Node): number => {
+export const compareNodes = (a: Node, b: Node): number => {
   const tuples = compareTuples(a, b);
 
   if (tuples !== 0) {
@@ -80,10 +87,10 @@ export const compareTimestamp = (a: number, b: number): number => a - b;
 /**
  * Returns the index of the first nodes which is greater than or equal to the given tuple.
  * If no nodes exist which are greater than or equal to the given tuple then it returns the last index.
- * 
- * @param nodes 
- * @param tuple 
- * @returns 
+ *
+ * @param nodes
+ * @param tuple
+ * @returns
  */
 export const findIndexClosestToGTE = (nodes: Node[], tuple: Tuple): number => {
   let index: number = 0;

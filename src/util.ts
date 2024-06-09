77a4191a7ff38f8b8b080d55332b8a083ff7a685
tabcat <tabcat00@proton.me>
2024-06-08 23:16:17 -0500

    chore: add utils for arrays

--- src/util.ts
+++ src/util.ts
@@ -1,24 +1,52 @@
 import { Prefix } from "./bucket";
 
 /**
+ * Returns the element indexed at i or throws if element is undefined.
+ *
+ * @param array
+ * @param i
+ * @returns
+ */
+export const ithElement = <T>(array: T[], i: number): T => {
+  const element = array[i];
+
+  if (element == null) {
+    throw new Error(
+      `did not find any elements at index ${i}. element is undefined`
+    );
+  }
+
+  return element;
+};
+
+/**
+ * Returns the first element of an array.
+ *
+ * @param array
+ * @returns
+ */
+export const firstElement = <T>(array: T[]): T => ithElement(array, 0);
+
+/**
  * Returns the last element of an array.
  *
  * @param array
  * @returns
  */
-export const lastElement = <T>(array: T[]): T => array[array.length - 1];
+export const lastElement = <T>(array: T[]): T =>
+  ithElement(array, array.length - 1);
 
 /**
  * Returns the reverse as a new array.
- * 
- * @param array 
- * @returns 
+ *
+ * @param array
+ * @returns
  */
 export const toReversed = <T>(array: Array<T>): Array<T> => {
-  const newArray = Array.from(array)
-  newArray.reverse()
-  return newArray
-}
+  const newArray = Array.from(array);
+  newArray.reverse();
+  return newArray;
+};
 
 export const prefixWithLevel = (prefix: Prefix, level: number): Prefix => ({
   ...prefix,

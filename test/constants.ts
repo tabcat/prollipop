6802cbe4cda9b918bb5b3008bc8020a304b2972b
tabcat <tabcat00@proton.me>
2024-07-02 12:54:09 -0400

    test: add test/constants.ts

---
+++ test/constants.ts
@@ -0,0 +1,46 @@
+import { encodeOptions } from "@ipld/dag-cbor";
+import * as cbor from "cborg";
+import { EncodedNode } from "../src/codec.js";
+import { DefaultBucket, DefaultNode } from "../src/impls.js";
+import {
+  sha256SyncHasher as syncHasher,
+  cborTreeCodec as treeCodec,
+} from "../src/index.js";
+import { Bucket, Prefix } from "../src/interface.js";
+import { CID } from "multiformats/cid";
+import { create as createMultihashDigest } from 'multiformats/hashes/digest'
+
+// nodes
+export const timestamp = 0;
+export const hash = new Uint8Array(4); // isBoundaryHash expects Uint8Array with length >= 4
+export const message = new Uint8Array(0);
+export const node = new DefaultNode(timestamp, hash, message);
+export const encodedNode: EncodedNode = [timestamp, hash, message];
+export const nodeBytes = cbor.encode(encodedNode, encodeOptions);
+export const nodeBytes2 = new Uint8Array([...nodeBytes, ...nodeBytes]);
+
+// buckets
+export type Mc = typeof treeCodec.code;
+export type Mh = typeof syncHasher.code;
+export const prefix: Prefix<Mc, Mh> = {
+  average: 1,
+  level: 0,
+  mc: treeCodec.code,
+  mh: syncHasher.code,
+};
+export const prefixBytes = cbor.encode(prefix, encodeOptions);
+export const bucketBytes = new Uint8Array([...prefixBytes, ...nodeBytes]);
+export const bucketHash = syncHasher.digest(bucketBytes).digest;
+export const bucketCid = CID.createV1(treeCodec.code, createMultihashDigest(syncHasher.code, bucketHash))
+export const bucket: Bucket<Mc, Mh> = new DefaultBucket(
+  prefix,
+  [node],
+  bucketBytes,
+  bucketHash,
+);
+export const emptyBucket: Bucket<Mc, Mh> = new DefaultBucket(
+  prefix,
+  [],
+  prefixBytes,
+  syncHasher.digest(prefixBytes).digest
+)

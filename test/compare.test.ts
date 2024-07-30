d702d5fba322f05cfaafb99ad7bd73dd873048ce
tabcat <tabcat00@proton.me>
2024-07-30 15:38:09 +0100

    test: add test for compareNodes

--- test/compare.test.ts
+++ test/compare.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { compareTimestamp, compareTuples } from "../src/compare.js";
+import { compareNodes, compareTimestamp, compareTuples } from "../src/compare.js";
 
 describe("compare", () => {
   describe("compareTimestamp", () => {
@@ -34,6 +34,38 @@ describe("compare", () => {
     it("returns 0 if the tuples are identical", () => {
       expect(compareTuples({ timestamp: 1, hash: new Uint8Array([1]) }, { timestamp: 1, hash: new Uint8Array([1]) })).to.equal(0)
       expect(compareTuples({ timestamp: 2, hash: new Uint8Array([2]) }, { timestamp: 2, hash: new Uint8Array([2]) })).to.equal(0)
+    });
+  });
+
+  describe("compareNodes", () => {
+    it("returns the difference of the timestamp if they do not match", () => {
+      expect(
+        compareNodes(
+          { timestamp: 1, hash: new Uint8Array(), message: new Uint8Array() },
+          { timestamp: 2, hash: new Uint8Array(), message: new Uint8Array() },
+        ),
+      ).to.equal(-1)
+      expect(
+        compareNodes(
+          { timestamp: 2, hash: new Uint8Array(), message: new Uint8Array() },
+          { timestamp: 1, hash: new Uint8Array(), message: new Uint8Array() },
+        ),
+      ).to.equal(1)
+    });
+
+    it("returns the order of the hashes if they do not match", () => {
+      expect(compareNodes({ timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array(1) }, { timestamp: 1, hash: new Uint8Array([2]) , message: new Uint8Array() })).to.equal(-1)
+      expect(compareNodes({ timestamp: 1, hash: new Uint8Array([2]), message: new Uint8Array(2) }, { timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array() })).to.equal(1)
+    });
+
+    it("returns the order of the messages if the tuples are identical", () => {
+      expect(compareNodes({ timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array(1) }, { timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array(2) })).to.equal(-1)
+      expect(compareNodes({ timestamp: 2, hash: new Uint8Array([2]), message: new Uint8Array(2) }, { timestamp: 2, hash: new Uint8Array([2]), message: new Uint8Array(1) })).to.equal(1)
+    });
+
+    it("returns 0 if the messsage are identical", () => {
+      expect(compareNodes({ timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array(1) }, { timestamp: 1, hash: new Uint8Array([1]), message: new Uint8Array(1) })).to.equal(0)
+      expect(compareNodes({ timestamp: 2, hash: new Uint8Array([2]), message: new Uint8Array(2) }, { timestamp: 2, hash: new Uint8Array([2]), message: new Uint8Array(2) })).to.equal(0)
     });
   });
 });

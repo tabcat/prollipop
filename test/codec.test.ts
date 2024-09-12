3465ed0b1f4913070b0262dc9f6155c5e269d061
tabcat <tabcat00@proton.me>
2024-09-12 15:40:22 +0100

    test: rewrite codec.test.ts

--- test/codec.test.ts
+++ test/codec.test.ts
@@ -1,56 +1,87 @@
+import { encode } from "@ipld/dag-cbor";
 import { describe, expect, it } from "vitest";
-import {
-  decodeBucket,
-  decodeNodeFirst,
-  encodeBucket,
-  encodeNode,
-  handleBuffer,
-} from "../src/codec.js";
-import {
-  bucket,
-  bucketBytes,
-  hash,
-  message,
-  node,
-  nodeBytes,
-  nodeBytes2,
-  prefix,
-  timestamp,
-} from "./helpers/constants.js";
+import { decodeBucket, encodeBucket } from "../src/codec.js";
+import { emptyBucket, encodedEmptyBucket } from "./helpers/constants.js";
+
+const { average, level, nodes } = emptyBucket;
 
 describe("codec", () => {
-  describe("handleBuffer", () => {
-    it("turns a buffer into a byte array", () => {
-      const buffer = new ArrayBuffer(0);
-      expect(handleBuffer(buffer)).to.deep.equal(new Uint8Array(0));
-    });
-  });
-
-  describe("encodeNode", () => {
-    it("turns a node into a byte array", () => {
-      expect(encodeNode(timestamp, hash, message)).to.deep.equal(nodeBytes);
-    });
-  });
-
-  describe("decodeNodeFirst", () => {
-    it("turns a byte array into [node class, byte array] tuple", () => {
-      expect(decodeNodeFirst(nodeBytes)).to.deep.equal([
-        node,
-        new Uint8Array(),
-      ]);
-      expect(decodeNodeFirst(nodeBytes2)).to.deep.equal([node, nodeBytes]);
-    });
-  });
-
   describe("encodeBucket", () => {
-    it("turns a bucket into a byte array", () => {
-      expect(encodeBucket(prefix, [node])).to.deep.equal(bucketBytes);
+    it("encodes a bucket", () => {
+      expect(encodeBucket(average, level, nodes)).to.deep.equal(
+        encodedEmptyBucket,
+      );
     });
   });
 
   describe("decodeBucket", () => {
-    it("turns a byte array into a bucket", () => {
-      expect(decodeBucket(bucketBytes)).to.deep.equal(bucket);
+    it("decodes a bucket", () => {
+      expect(
+        decodeBucket(encodedEmptyBucket, { average, level }),
+      ).to.deep.equal(emptyBucket);
+    });
+
+    it("throws when expected average does not match", () => {
+      expect(() =>
+        decodeBucket(encodedEmptyBucket, { average: -1, level }),
+      ).toThrow();
+    });
+
+    it("throws when expected level does not match", () => {
+      expect(() =>
+        decodeBucket(encodedEmptyBucket, { average, level: -1 }),
+      ).toThrow();
+    });
+
+    it("throws when decoded bucket is not an object", () => {
+      expect(() => decodeBucket(encode(null), { average, level })).toThrow(
+        "Expected bucket to be an object.",
+      );
+    });
+
+    it("throws when decoded average is not a number", () => {
+      expect(() => decodeBucket(encode({}), { average, level })).toThrow(
+        "Expected prefix average field to be a number.",
+      );
+    });
+
+    it("throws when decoded level is not a number", () => {
+      expect(() =>
+        decodeBucket(encode({ average }), { average, level }),
+      ).toThrow("Expected prefix level field to be a number.");
+    });
+
+    it("throws when decoded nodes is not an array", () => {
+      expect(() =>
+        decodeBucket(encode({ average, level }), { average, level }),
+      ).toThrow("Expected bucket nodes field to be a number.");
+    });
+
+    it("throws when decoded nodes contains an invalid timestamp", () => {
+      expect(() =>
+        decodeBucket(encode({ average, level, nodes: [[null, null, null]] }), {
+          average,
+          level,
+        }),
+      ).toThrow("Expected node timestamp field to be a number.");
+    });
+
+    it("throws when decoded nodes contains an invalid hash", () => {
+      expect(() =>
+        decodeBucket(encode({ average, level, nodes: [[0, null, null]] }), {
+          average,
+          level,
+        }),
+      ).toThrow("Expected node hash field to be a byte array.");
+    });
+
+    it("throws when decoded nodes contains an invalid message", () => {
+      expect(() =>
+        decodeBucket(
+          encode({ average, level, nodes: [[0, new Uint8Array(), null]] }),
+          { average, level },
+        ),
+      ).toThrow("Expected node message field to be a byte array.");
     });
   });
 });

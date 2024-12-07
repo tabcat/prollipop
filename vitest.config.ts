03f507f6bf7a846f45135f1e9d2a0d44b363162b
tabcat <tabcat00@proton.me>
2024-12-07 13:45:13 +0700

    test(env): remove TREE_ENTRIES_MAX

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  // Vitest configuration options here
-  define: {
-    TREE_ENTRIES_MAX: JSON.stringify(process.env.TREE_ENTRIES_MAX ?? 3000),
-  },
-});
+export default defineConfig({});

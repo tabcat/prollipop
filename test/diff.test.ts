78df3cd7818ac28540772c82431714576d7a49f3
tabcat <tabcat00@proton.me>
2024-12-08 10:40:28 +0700

    test(diff): simplify test

--- test/diff.test.ts
+++ test/diff.test.ts
@@ -1,87 +1,60 @@
-import { diff as orderedDiff } from "@tabcat/sorted-sets/difference";
-import { pairwiseTraversal } from "@tabcat/sorted-sets/util";
-import { describe, expect, it } from "vitest";
-import { compareBuckets, compareBytes, compareTuples } from "../src/compare.js";
-import { BucketDiff, EntryDiff, diff } from "../src/diff.js";
-import { Entry, ProllyTree } from "../src/interface.js";
-import {
-  blockstore,
-  emptyBucket,
-  trees,
-  treesToStates,
-} from "./helpers/constants.js";
+import { ExclusiveDiff } from "@tabcat/sorted-sets/difference";
+import { describe, expect, it, vi } from "vitest";
+import "../src/boundary.js";
+import { ProllyTreeDiff, diff } from "../src/diff.js";
+import { loadBucket } from "../src/utils.js";
+import { blockstore, emptyBucket } from "./helpers/constants.js";
+import { oddTree, oddTreeEntries } from "./helpers/odd-tree.js";
 
-export async function checkDiffs(
-  tree1: ProllyTree,
-  tree2: ProllyTree,
-): Promise<void> {
-  const actualEntryDiffs: EntryDiff[] = [];
-  const actualBucketDiffs: BucketDiff[] = [];
-
-  for await (const { entries, buckets } of diff(blockstore, tree1, tree2)) {
-    for (const diff of entries) {
-      actualEntryDiffs.push(diff);
-    }
-
-    for (const diff of buckets) {
-      actualBucketDiffs.push(diff);
-    }
-  }
-
-  const expectedEntryDiffs = Array.from(
-    orderedDiff(
-      treesToStates.get(tree1)!.entries,
-      treesToStates.get(tree2)!.entries,
-      compareTuples,
-      (a: Entry, b: Entry) => compareBytes(a.val, b.val) !== 0,
-    ),
-  );
-  const expectedBucketDiffs = Array.from(
-    orderedDiff(
-      treesToStates.get(tree1)!.buckets,
-      treesToStates.get(tree2)!.buckets,
-      compareBuckets,
-    ),
-  );
-
-  expect(actualEntryDiffs.length).to.equal(expectedEntryDiffs.length);
-  for (const [actualDiff, expectedDiff] of pairwiseTraversal(
-    actualEntryDiffs,
-    expectedEntryDiffs,
-    () => 0,
-  )) {
-    expect(actualDiff).to.deep.equal(expectedDiff);
-  }
-
-  expect(actualBucketDiffs.length).to.equal(expectedBucketDiffs.length);
-  for (const [actualDiff, expectedDiff] of pairwiseTraversal(
-    actualBucketDiffs,
-    expectedBucketDiffs,
-    () => 0,
-  )) {
-    expect(actualDiff).to.deep.equal(expectedDiff);
-  }
-}
+vi.mock("../src/boundary.js");
 
 describe("diff", () => {
-  it("yields nothing for two empty trees", async () => {
-    // both trees empty
-    for await (const _ of diff(
-      blockstore,
-      { root: emptyBucket },
-      { root: emptyBucket },
-    )) {
-      // this line is never reached
-      expect.assertions(0);
+  it("yields nothing for two identical trees", async () => {
+    for await (const _ of diff(blockstore, oddTree, oddTree)) {
+      expect.fail();
     }
   });
 
-  for (const tree1 of trees) {
-    const tree1Name = treesToStates.get(tree1)!.name;
-    for (const tree2 of trees) {
-      const tree2Name = treesToStates.get(tree2)!.name;
-      it(`yields diff of ${tree1Name} and ${tree2Name} trees`, async () =>
-        checkDiffs(tree1, tree2));
+  it("yields the different entries and buckets", async () => {
+    const removed = <T>(e: T): ExclusiveDiff<T> => [e, null];
+
+    const expected: ProllyTreeDiff[] = [
+      {
+        entries: oddTreeEntries.slice(0, 1).map(removed),
+        buckets: [[null, emptyBucket]],
+      },
+      {
+        entries: oddTreeEntries.slice(1).map(removed),
+        buckets: [
+          [
+            await loadBucket(blockstore, oddTree.root.entries[0]!.val, false),
+            null,
+          ],
+          [
+            await loadBucket(blockstore, oddTree.root.entries[1]!.val, false),
+            null,
+          ],
+          [
+            await loadBucket(blockstore, oddTree.root.entries[2]!.val, true),
+            null,
+          ],
+          [oddTree.root, null],
+        ],
+      },
+    ];
+
+    let count = 0;
+    for await (const { entries, buckets } of diff(blockstore, oddTree, {
+      root: emptyBucket,
+    })) {
+      try {
+        expect(entries).to.deep.equal(expected[count]!.entries);
+        expect(buckets).to.deep.equal(expected[count]!.buckets);
+        count++;
+      } catch (e) {
+        console.error(e);
+        throw new Error("failed diff test at count: " + count);
+      }
     }
-  }
+  });
 });

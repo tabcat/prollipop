e8dd5b4468b597e227a4504b07968a2dc47e12a0
tabcat <tabcat00@proton.me>
2024-12-21 18:26:06 +0700

    docs(boundary): clean up jsdoc

--- src/boundary.ts
+++ src/boundary.ts
@@ -1,7 +1,7 @@
 /**
- * implements similar boundary resolution as okra-js but only considers keys
- * https://github.com/canvasxyz/okra-js/blob/d3490b2c988564af2aca07996fad7b0b859a2ddd/packages/okra/src/Builder.ts#L114
+ * Boundary resolution similar to okra-js but considers (key, level) instead of (key, value).
  *
+ * @see {@link https://github.com/canvasxyz/okra-js/blob/d3490b2c988564af2aca07996fad7b0b859a2ddd/packages/okra/src/Builder.ts#L114|okra-js implementation}
  */
 
 import { encode } from "@ipld/dag-cbor";
@@ -18,8 +18,11 @@ export interface IsBoundary {
 
 /**
  * Returns a function that determines if an entry is a boundary.
+ *
  * Expects average to be a positive integer between 1 and 2^32 - 1;
  * more realistically around 30 as it represents the desired number of keys per bucket.
+ * The average is used to calculate the limit as MAX_UINT32 / average.
+ * The boundary is determined by hashing the level and tuple and then checking if the first four bytes fall below the limit.
  *
  * @param average
  * @param level
@@ -32,8 +35,6 @@ export const createIsBoundary: CreateIsBoundary = (
   const limit = Number(MAX_UINT32 / BigInt(average));
 
   return ({ seq, key }: Tuple) => {
-    // boundary is determined by the level and tuple
-    // this keeps boundaries consistent across different values
     const digest = sha256(encode([level, seq, key]));
     return (
       new DataView(digest.buffer, digest.byteOffset, 4).getUint32(0) < limit

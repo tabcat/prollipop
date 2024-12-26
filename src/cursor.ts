041ce4b60fe5f94c663e63ca0dd430b3ee54f185
tabcat <tabcat00@proton.me>
2024-12-26 11:15:21 +0800

    refactor(interface): export Cursor

--- src/cursor.ts
+++ src/cursor.ts
@@ -3,7 +3,7 @@ import type { Blockstore } from "interface-blockstore";
 import { TupleRange } from "./codec.js";
 import { compareBytes, compareTuples } from "./compare.js";
 import { minTuple } from "./constants.js";
-import { Bucket, Entry, ProllyTree, Tuple } from "./interface.js";
+import { Bucket, Cursor, Entry, ProllyTree, Tuple } from "./interface.js";
 import { loadBucket } from "./utils.js";
 
 interface CursorState {
@@ -32,79 +32,6 @@ const createCursorState = (
     isLocked: false,
   };
 };
-
-export interface Cursor {
-  /**
-   * Returns the current level of the cursor.
-   */
-  level(): number;
-  /**
-   * Returns the root level of the tree.
-   */
-  rootLevel(): number;
-
-  /**
-   * Returns the index of the current entry in the bucket. If index is -1 the bucket is empty and current() will throw an error.
-   */
-  index(): number;
-  /**
-   * Returns the current entry in the bucket. If the bucket is empty this method will throw an error.
-   */
-  current(): Entry;
-
-  /**
-   * Returns an array of buckets from root to current level.
-   */
-  buckets(): Bucket[];
-  /**
-   * Returns the current bucket. The last bucket in the array returned by the buckets() method.
-   */
-  currentBucket(): Bucket;
-
-  /**
-   * Moves the cursor to the next tuple on the current level.
-   */
-  next(level?: number): Promise<void>;
-
-  /**
-   * Moves the cursor to the beginning of the next bucket on the current level.
-   */
-  nextBucket(level?: number): Promise<void>;
-
-  /**
-   * Moves the cursor to the next tuple on the current level.
-   * If the supplied tuple is less than or equal to the current tuple, the cursor will not be moved.
-   */
-  nextTuple(tuple: Tuple, level?: number): Promise<void>;
-
-  /**
-   * Jumps the cursor from root to the tuple or parent tuple at level. This is not a move operation.
-   */
-  jumpTo(tuple: Tuple, level?: number): Promise<void>;
-
-  /**
-   * Returns true or false depending on whether the cursor is at the tail bucket for the level.
-   */
-  isAtTail(): boolean;
-  /**
-   * Returns true or false depending on whether the cursor is at the head bucket for the level.
-   */
-  isAtHead(): boolean;
-
-  /**
-   * Returns true or false depending on whether the cursor is currently being incremented.
-   */
-  locked(): boolean;
-  /**
-   * Returns true or false depending on whether the cursor has reached the end of the tree.
-   */
-  done(): boolean;
-
-  /**
-   * Returns a clone of the cursor instance.
-   */
-  clone(): Cursor;
-}
 
 function createCursorFromState(state: CursorState): Cursor {
   return {
--- src/interface.ts
+++ src/interface.ts
@@ -1,4 +1,7 @@
 export interface Tuple {
+  /**
+   * Sequence number. Can be used to attach order to the key.
+   */
   readonly seq: number;
   readonly key: Uint8Array;
 }
@@ -37,15 +40,103 @@ export interface Bucket extends Prefix {
 }
 
 export interface Addressed {
+  /**
+   * The serialized bucket.
+   */
   readonly bytes: Uint8Array;
+  /**
+   * The digest of the bucket.
+   */
   readonly digest: Uint8Array;
 }
 
 export interface Context {
+  /**
+   * Whether the bucket is the tail of the tree.
+   */
   readonly isTail: boolean;
+  /**
+   * Whether the bucket is the head of the tree.
+   */
   readonly isHead: boolean;
 }
 
 export interface ProllyTree {
   root: Bucket;
+}
+
+/**
+ * A cursor enables ordered traversal of a prolly-tree.
+ */
+export interface Cursor {
+  /**
+   * Returns the current level of the cursor.
+   */
+  level(): number;
+  /**
+   * Returns the root level of the tree.
+   */
+  rootLevel(): number;
+
+  /**
+   * Returns the index of the current entry in the bucket. If index is -1 the bucket is empty and current() will throw an error.
+   */
+  index(): number;
+  /**
+   * Returns the current entry in the bucket. If the bucket is empty this method will throw an error.
+   */
+  current(): Entry;
+
+  /**
+   * Returns an array of buckets from root to current level.
+   */
+  buckets(): Bucket[];
+  /**
+   * Returns the current bucket. The last bucket in the array returned by the buckets() method.
+   */
+  currentBucket(): Bucket;
+
+  /**
+   * Moves the cursor to the next tuple on the current level.
+   */
+  next(level?: number): Promise<void>;
+
+  /**
+   * Moves the cursor to the beginning of the next bucket on the current level.
+   */
+  nextBucket(level?: number): Promise<void>;
+
+  /**
+   * Moves the cursor to the next tuple on the current level.
+   * If the supplied tuple is less than or equal to the current tuple, the cursor will not be moved.
+   */
+  nextTuple(tuple: Tuple, level?: number): Promise<void>;
+
+  /**
+   * Jumps the cursor from root to the tuple or parent tuple at level. This is not a move operation.
+   */
+  jumpTo(tuple: Tuple, level?: number): Promise<void>;
+
+  /**
+   * Returns true or false depending on whether the cursor is at the tail bucket for the level.
+   */
+  isAtTail(): boolean;
+  /**
+   * Returns true or false depending on whether the cursor is at the head bucket for the level.
+   */
+  isAtHead(): boolean;
+
+  /**
+   * Returns true or false depending on whether the cursor is currently being incremented.
+   */
+  locked(): boolean;
+  /**
+   * Returns true or false depending on whether the cursor has reached the end of the tree.
+   */
+  done(): boolean;
+
+  /**
+   * Returns a clone of the cursor instance.
+   */
+  clone(): Cursor;
 }

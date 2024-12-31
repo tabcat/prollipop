c8828b1a6fe06243ef02e81878cdb05228b2ad02
tabcat <tabcat00@proton.me>
2024-12-31 18:27:38 +0800

    test(stress): trees average = 3; randomized removes 1 of 3

--- test/stress/trees.ts
+++ test/stress/trees.ts
@@ -1,6 +1,5 @@
 import { firstElement } from "@tabcat/ith-element";
 import { compareBuckets } from "../../src/compare.js";
-import { DEFAULT_AVERAGE } from "../../src/constants.js";
 import { DefaultProllyTree } from "../../src/impls.js";
 import { Bucket, Entry, ProllyTree } from "../../src/interface.js";
 import {
@@ -9,7 +8,12 @@ import {
 } from "../helpers/build-tree.js";
 import { blockstore } from "../helpers/constants.js";
 
-export const treeEntriesMax = 300;
+// average is low so that there are more unique tree shapes with fewer entries
+const average = 3;
+// randomized trees remove 1 of 3 entries
+const random_threshold = 1 / 3;
+
+export const treeEntriesMax = 3000;
 export const treeEntriesHalf = Math.floor(treeEntriesMax / 2);
 export const treeEntriesThird = Math.floor(treeEntriesMax / 3);
 
@@ -44,7 +48,9 @@ for (const [name, ids] of Array.from(Object.entries(namedTreeIds))) {
     continue;
   }
 
-  namedTreeIds["randomized-" + name] = ids.filter(() => Math.random() - 0.5);
+  namedTreeIds["randomized-" + name] = ids.filter(
+    () => Math.random() > random_threshold,
+  );
 }
 
 interface TreeState {
@@ -63,7 +69,7 @@ for (const [name, ids] of Array.from(Object.entries(namedTreeIds))) {
   }
 
   const entries = createProllyTreeEntries(ids);
-  const state = buildProllyTree(blockstore, DEFAULT_AVERAGE, entries);
+  const state = buildProllyTree(blockstore, average, entries);
   const buckets = state.flat().sort(compareBuckets);
   const tree = new DefaultProllyTree(firstElement(firstElement(state)));
 

5e8021c9d1fa3d6a906e088c1894d89e123a6c76
tabcat <tabcat00@proton.me>
2025-01-08 17:58:54 +0800

    chore: run stress tests by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vitest/config";
 
-const stress = process.env.TEST_STRESS === "true";
-
 export default defineConfig({
   test: {
-    include: !stress ? ["./test/*.test.ts"] : ["./test/stress/*.test.ts"],
-    isolate: stress,
+    isolate: true,
   },
 });

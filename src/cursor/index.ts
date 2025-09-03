78b0d62cd39cfb94b6ce4cc573052059f76d7100
tabcat <tabcat00@proton.me>
2025-09-02 22:43:33 -0500

    docs(cursor): add jsdoc to cursor

--- src/cursor/index.ts
+++ src/cursor/index.ts
@@ -24,6 +24,7 @@ export interface Cursor extends CursorState {}
 /**
  * Creates a cursor.
  * Cursors are used to move around the tree and read.
+ * Once a cursor is done (cursor.isDone === true) it cannot be moved left or right anymore, only up and down.
  *
  * @param blockstore
  * @param tree
@@ -38,20 +39,52 @@ export const createCursor = (
   return state;
 };
 
+/**
+ * Creates a copy of the cursor.
+ *
+ * @param cursor - from createCursor
+ * @returns
+ */
 export const cloneCursor = (cursor: Cursor): Cursor => cloneCursorState(cursor);
 
+/**
+ * Moves the cursor to the next entry on the given level.
+ * If the current entry on the given level is already the last entry on the level the cursor is set to done.
+ *
+ * @param state
+ * @param level
+ * @returns
+ */
 export const next = async (
   state: Cursor,
   level: number = getCurrentLevel(state),
 ): Promise<void> =>
   preMove(state, level, (state, level) => nextAtLevel(state, level, false));
 
+/**
+ * Moves the cursor to the next bucket on the given level.
+ * If the current bucket on the given level is already the last bucket on the level the cursor is set to done.
+ *
+ * @param state - from createCursor
+ * @param level
+ * @returns
+ */
 export const nextBucket = async (
   state: Cursor,
   level: number = getCurrentLevel(state),
 ): Promise<void> =>
   preMove(state, level, (state, level) => nextAtLevel(state, level, true));
 
+/**
+ * Moves the cursor to the next key that is greater than or equal to the given key on the given level.
+ * If the current key is already greater than or equal the cursor is not moved.
+ * If the given key is greater than all keys in the tree then the cursor is moved to the last entry on the given level and set to done.
+ *
+ * @param state - from createCursor
+ * @param key
+ * @param level
+ * @returns
+ */
 export const nextKey = async (
   state: Cursor,
   key: ComparableKey,
@@ -60,9 +93,11 @@ export const nextKey = async (
   preMove(state, level, (state, level) => nextKeyAtLevel(state, key, level));
 
 /**
+ * JumpTo will set the cursor to any node on any level if the cursor is not done.
+ * This is unlike the other methods which can only move the cursor to the right.
+ * This is because JumpTo will set the cursor to root and traverse down.
  *
- *
- * @param state
+ * @param state - from createCursor
  * @param key
  * @param level
  * @returns
